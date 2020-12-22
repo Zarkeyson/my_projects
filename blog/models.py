821e499317a3364c793f08839b2b394d71383ea1
Zarkeyson <djurdjev.zarko@gmail.com>
2020-12-22 22:57:32 +0100

    New database, PostgreSQL

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager() #The default manager
     published = PublishedManager() #Our custom manager
-    tags = TaggableManager()
+    tags = TaggableManager() # Omogucuje dodavanje,preuzimanje i uklanjanje oznaka iz Posta
 
 
     class Meta:
